5cb077b8ab0e1997ec3ad74b255b1f4b7a25d448
haneena <haneenakt123@gmail.com>
2026-06-11 13:52:45 -0930

    second commit add python file

---
+++ python.py
@@ -0,0 +1,32 @@
+# a=float(input("enter a number:"))
+# b=float(input("enter a number:"))
+# c=float(input("enter a number:"))
+# d=float(input("enter a number:"))
+# if a>b and a>c and a>d:
+#    print("the largest number is:",a)
+# elif b>a and b>c and b>d:
+#    print("the largest number is:",b)
+# elif c>a and c>b and c>d:
+#    print("the largest number is:",c)
+# elif d>a and d>b and d>c:
+#    print("the largest number is:",d)
+# else:
+#    print("all are equal")
+
+
+name=input("name:")
+a=float(input("enter the python mark:"))
+b=float(input("enter the physics mark:"))
+c=float(input("enter the maths mark:"))
+total=a+b+c
+percentage=total/300*100
+print("total=",total)
+print("percentage=",percentage)
+if percentage>=45:
+    print(name,"is passed")
+elif percentage>=0 and percentage<45:
+    print(name,"is failed")
+else:
+    print("invalid")
+
+
